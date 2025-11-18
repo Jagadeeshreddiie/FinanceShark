e5474c5c4f1740a5a6326a7633e29526310d7302
Jagadeeshreddiie <jagadeeshreddii4u@gmail.com>
2025-11-19 01:01:03 +0530

    Finance Project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState(0);
+  return (
+    <>
+      <div style={{ textAlign: "center", color: "red" }}>
+        <button onClick={() => setValue(value + 1)}>Click to increase</button>
+        <p>{value}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
